20c5c1683df89e272ee979332a4a9e4f15ec3fac
417170808@qq.com <you@example.com>
2021-03-06 17:57:23 +0800

    add command

---
+++ app/Http/Controllers/SysUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SysUser;
+use Illuminate\Http\Request;
+
+class SysUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SysUser  $sysUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SysUser $sysUser)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SysUser  $sysUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SysUser $sysUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SysUser  $sysUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SysUser $sysUser)
+    {
+        //
+    }
+}
